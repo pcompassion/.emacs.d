b93de6965c7b709124611f8bff3ce0b10376b83d
Eugene Kim <p.compassion@gmail.com>
2014-01-17 12:10:48 +0900

    moving git -> elpa

---
+++ elpa/dummy-h-mode-20130824.1159/dummy-h-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dummy-h-mode" "20130824.1159" "switch major mode to c-/c++-/objc-mode on .h file" (quote nil))
---
+++ elpa/popup-20131230.544/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21207 54684
+;;;;;;  929811 545000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
