8b2b9867941df61cf0db9d2ab4380ffa119393ec
pcompassion <p.compassion@gmail.com>
2017-01-22 21:50:27 +0900

    fix

--- init.el
+++ init.el
@@ -312,7 +312,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-grep-file ((t (:foreground "blue1" :underline t))))
+ '(helm-grep-file ((t (:foreground "green" :underline t))))
  '(helm-ls-git-modified-not-staged-face ((t (:foreground "orange"))))
  '(helm-selection ((t (:background "blue" :foreground "white"))))
  '(highlight-indentation-face ((t nil)))
